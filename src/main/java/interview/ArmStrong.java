f51c17a03a1fb208afc7b64ea4fb42996f1e228e
Siddhu9966 <saisiddhardha54@gmail.com>
2024-05-23 19:20:48 +0530

    Added Armstrong and sortingminmax

--- src/main/java/interview/ArmStrong.java
+++ src/main/java/interview/ArmStrong.java
@@ -9,8 +9,8 @@ public class ArmStrong {
         while (originalNumber != 0)
         {
             remainder = originalNumber % 10;
-            result += Math.pow(remainder, 3);
-            originalNumber /= 10;
+            result = (int) (result+ Math.pow(remainder, 3));
+            originalNumber = originalNumber/10;
         }
 
         if(result == number)
@@ -19,3 +19,15 @@ public class ArmStrong {
             System.out.println(number + " is not an Armstrong number.");
     }
 }
+//153 --> 1^3 + 5^3+3^3 = 1 + 125+27 = 153
+// 1*1*1  = 1
+//5*5*5 = 125
+//3*3*3 = 27
+
+//20895 = 2*2*2 = 8
+/*
+10)371(37
+    370
+   -------
+       1
+ */
