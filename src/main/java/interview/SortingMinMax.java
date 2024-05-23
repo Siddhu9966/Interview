503788fdc28ee18208881434f44945e2c07b5df9
Siddhu9966 <saisiddhardha54@gmail.com>
2024-05-23 15:46:09 +0530

    Added Armstrong and sortingminmax

--- src/main/java/interview/SortingMinMax.java
+++ src/main/java/interview/SortingMinMax.java
@@ -1,5 +1,7 @@
 package interview;
 
+import java.util.Arrays;
+
 public class SortingMinMax {
     static int min(int []arr){
         int min=500;
@@ -13,41 +15,37 @@ public class SortingMinMax {
     static int max(int arr[]){
         int max=0;
         for(int i=0;i<arr.length;i++){
-            if(arr[i]>max){
+            if(arr[i]>max){ //50>0
                 max=arr[i];
             }
         }
         return max;
     }
     static void sort(int []arr){
-        int a=0;
+        int temp=0;
         for (int i = 0; i < arr.length; ++i)
         {
-
             for (int j = i + 1; j < arr.length; ++j)
             {
-
                 if (arr[i] > arr[j])
                 {
-
-                    a =  arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = a;
-
+                    temp =  arr[i]; //temp = 5
+                    arr[i] = arr[j];  // arr[0]=4
+                    arr[j] = temp; //arr[i] = 5
                 }
-
             }
-
         }
         for(int i=0;i<arr.length;i++)
         {
             System.out.print(arr[i]+" ");
         }
+        System.out.println("");
     }
     public static void main(String[] args){
         int arr[]= {5,9,6,7,8,6,4,5,25,69,1};
         System.out.println("Min value:" +min(arr));
         System.out.println("Max value:" +max(arr));
         sort(arr);
+
     }
 }
